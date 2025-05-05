22fa4ac5e3f7b935658ebbb11d809fbc9c43e5d0
Isaac Jiang <isaac.jiang66@gmail.com>
2025-05-05 11:48:56 -0400

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,36 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/isaac">Isaac</RouterLink>
-        <RouterLink to="/raj">Raj</RouterLink>
-        <RouterLink to="/Rick">Rick</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <v-app class="app">
+    <v-app-bar app color="grey-darken-3" dark>
+      <v-toolbar-title class="text-h6">Portal Co-op Team Summer 2025</v-toolbar-title>
 
-  <RouterView />
+      <v-spacer />
+
+      <v-btn to="/" variant="text" class="text-white" exact :ripple="false"> Home </v-btn>
+      <v-btn to="/isaac" variant="text" class="text-white" exact :ripple="false"> Isaac </v-btn>
+      <v-btn to="/raj" variant="text" class="text-white" exact :ripple="false"> Raj </v-btn>
+      <v-btn to="/rick" variant="text" class="text-white" exact :ripple="false"> Rick </v-btn>
+    </v-app-bar>
+
+    <v-main class="mainpage align-center justify-center">
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
+.mainpage {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5f5f5;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,19 @@
 // plugins/vuetify.js
-import 'vuetify/styles'              // ← this line pulls in all of Vuetify’s CSS
+import 'vuetify/styles' // ← this line pulls in all of Vuetify’s CSS
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 export default createVuetify({
+  icons: {
+    defaultSet: 'mdi', // Specify the icon set
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
   components,
   directives,
 })
