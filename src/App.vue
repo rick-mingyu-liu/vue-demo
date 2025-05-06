3ec967f2fb908dae6bee99d5f8f18857d5e204c2
Isaac Jiang <isaac.jiang66@gmail.com>
2025-05-06 15:52:20 -0400

    fixed footer

--- src/App.vue
+++ src/App.vue
@@ -59,13 +59,6 @@ watch(
       </v-btn>
     </v-main>
     <v-footer class="myfooter d-flex align-center justify-center ga-2 flex-wrap flex-grow-1 py-3">
-    <v-btn
-      v-for="link in links"
-      :key="link"
-      :text="link"
-      variant="text"
-      rounded
-    ></v-btn>
 
     <div class="flex-1-0-100 text-center mt-2">
       {{ new Date().getFullYear() }} — Isaac Jiang, Rick Liu, Raj Shah
